ab78b2d34a1c1b5fe1758e001215f3e6bbb413e0
sanwakuwaku <sanwaku2@gmail.com>
2022-08-16 17:07:07 +0900

    (4)テーマを定義する

---
+++ ThemingCodelab/app/src/main/java/com/codelab/theming/ui/start/theme/Color.kt
@@ -0,0 +1,11 @@
+package com.codelab.theming.ui.start.theme
+
+import androidx.compose.ui.graphics.Color
+
+// 色を定義する際は意味的な名前ではなく色の値に基づいて命名する
+// 複数のテーマで利用できるようにするため
+val Red700 = Color(0xffdd0d3c)
+val Red800 = Color(0xffd00036)
+val Red900 = Color(0xffc20029)
+val Red200 = Color(0xfff297a2)
+val Red300 = Color(0xffea6d7e)
---
+++ ThemingCodelab/app/src/main/java/com/codelab/theming/ui/start/theme/Theme.kt
@@ -0,0 +1,44 @@
+package com.codelab.theming.ui.start.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+// xxxColors関数を使ってColorsを作成する
+// 適切なデフォルト値が提供されるためマテリアルカラーパレットの構成色を全て指定する必要はない。
+// ブランドでプライマリとセカンダリの色を分けていない場合は両方同じ色で構わない。
+private val LightColors = lightColors(
+    primary = Red700,
+    primaryVariant = Red900,
+    onPrimary = Color.White,
+    secondary = Red700,
+    secondaryVariant = Red900,
+    onSecondary = Color.White,
+    error = Red800
+)
+
+// ダークモードの色
+private val DarkColors = darkColors(
+    primary = Red300,
+    primaryVariant = Red700,
+    onPrimary = Color.Black,
+    secondary = Red300,
+    onSecondary = Color.Black,
+    error = Red200
+)
+
+@Composable
+fun JetnewsTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    MaterialTheme(
+        colors = if (darkTheme) DarkColors else LightColors,
+        typography = JetnewsTypography,
+        shapes = JetnewsShapes,
+        content = content
+    )
+}
