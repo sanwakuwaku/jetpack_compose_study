fb6c431b8b7dc321282669d8bc84362fc2401c5e
sanwakuwaku <sanwaku2@gmail.com>
2022-08-17 12:26:13 +0900

    (5) コンポーズ可能な関数のメモリ

---
+++ BasicStateCodelab/app/src/main/java/com/sanwaku2/basicstatecodelab/WaterCounter.kt
@@ -0,0 +1,38 @@
+package com.sanwaku2.basicstatecodelab
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun WaterCounter(modifier: Modifier = Modifier) {
+    Column(modifier = modifier.padding(16.dp)) {
+        // State型とMutableState型でComposeが状態を監視できる
+        // valueが変更された時にそれを読み取る関数への再コンポーズがトリガーされる
+
+        // rememberを使用して再コンポーズ後も変数を保持できる
+        // オブジェクトないのプライベートvalプロパティと同じように、コンポジション内に単一のオブジェクトを
+        // 保存するメカニズムと考えることができる
+
+        // val count: MutableState<Int> = remember { mutableStateOf(0) }
+
+        // デリゲートプロパティ (by) を使用して簡素化できる
+        // valueプロパティを明示的に参照せずに間接的にcountを読み取ることができる
+        var count by remember { mutableStateOf(0) }
+        Text(text = "You've had ${count} glasses.")
+        Button(
+            onClick = { count++ },
+            modifier = Modifier.padding(top = 8.dp)
+        ) {
+            Text(text = "Add one")
+        }
+    }
+}
